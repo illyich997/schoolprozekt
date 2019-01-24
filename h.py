8733dd30816675f3050ef92627b28dd9ed458fb2
illyich997 <46988313+illyich997@users.noreply.github.com>
2019-01-24 12:08:08 +0530

    Add files via upload

---
+++ h.py
@@ -0,0 +1,4 @@
+a={"Super Deluxe":1000000,"Deluxe":10000,"Double":5000,"Regular":1000,"Luxury":100000}
+
+w={1:"Regular",2:"Double":3:"Deluxe":4:"Double Deluxe":5:"Luxury"}
+
---
+++ y.py
@@ -0,0 +1,28 @@
+print("              Galactic Internatonal Hotel")
+print("1. New Reservation")
+print("2. Prepare Bill")
+print("3. Display all rservations")
+print("4. Display existing reservations")
+print("5. Edit an existing reservation")
+print("6. Delete an existing reservations")
+print("7. Room service")
+print("8. Quit")
+a=int(input("Choose an option(1-8)"))
+for a==1:
+    b=input("Enter the first name:")
+    c=input("Enter the last name:")
+    d=input("E-Mail address:")
+    e=input("Contact number:")
+    f=(int(input("Check-in year:")) 
+    g=(int(input("Check-in month:"))
+    h=(int(input("Check-in day:"))
+    i=(int(input("Check-out year:"))
+    j=(int(input("Check-out month:"))
+    k=(int(input("Check-out day:")
+    print("Choose the room types:")
+           l=int(input("1. 
+    
+        
+           
+    
+    
